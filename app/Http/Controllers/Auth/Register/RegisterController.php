f9d60297fb1bfbe0f2b6df638597997a6fbf3733
Armen Matevosyan <aamatevosyan@edu.hse.ru>
2022-03-02 21:34:48 +0400

    chore: add register controller

--- app/Http/Controllers/Auth/Register/RegisterController.php
+++ app/Http/Controllers/Auth/Register/RegisterController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers\Auth\Register;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\Register\RegisterFinalizeRequest;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Hash;
+use Illuminate\Http\RedirectResponse;
+use Inertia\Inertia;
+use Inertia\Response as InertiaResponse;
+use Request;
 
 class RegisterController extends Controller
 {
-    //
+    public function create(Request $request, User $user): InertiaResponse
+    {
+        return Inertia::render('Auth/Finalize', ['uuid' => $user->uuid]);
+    }
+
+    public function store(RegisterFinalizeRequest $request, User $user): RedirectResponse
+    {
+        $validated = $request->validated();
+        $validated['password'] = Hash::make($validated['password']);
+
+        $user->update($validated);
+
+        return redirect()->route(RouteServiceProvider::HOME);
+    }
 }
