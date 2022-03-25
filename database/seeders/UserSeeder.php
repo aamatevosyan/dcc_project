6c458ecb8c69746522b93f4a25f45b2e0f7f9feb
Armen Matevosyan <aamatevosyan@edu.hse.ru>
2022-03-25 17:45:42 +0400

    add backpack admin panel

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'email' => 'admin@dcc.ru',
+            'password' => Hash::make('secret'),
+            'status' => User::STATUS_ACTIVE,
+        ]);
+
+        $admin->assign('admin');
+    }
+}
